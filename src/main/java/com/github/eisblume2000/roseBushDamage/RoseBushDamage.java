f0df83f29eebc1d5efda8be3f5c17699ad519b03
Eisblume-2000 <95316680+Eisblume-2000@users.noreply.github.com>
2025-01-10 19:26:06 +0100

    Intilazied git stuff

---
+++ src/main/java/com/github/eisblume2000/roseBushDamage/RoseBushDamage.java
@@ -0,0 +1,17 @@
+package com.github.eisblume2000.roseBushDamage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoseBushDamage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
